1f0a2e343c837bbfd0eae7c28b021a2d7d1d03e3
Collins Dada <140630132+Contractor-x@users.noreply.github.com>
2025-07-31 03:32:14 +0100

    Update app.js

--- frontend/pages/app.js
+++ frontend/pages/app.js
@@ -1,7 +1,7 @@
 // frontend/pages/_app.js
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
